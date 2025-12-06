fe415d3a78361005c2d25f403ad096aed60b856a
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-12-06 01:09:52 -0500

    refactor: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { ThemeProvider } from "./ThemeProvider";
+import { AppShell } from "./AppShell";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <AppShell />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
