e10dbcccc8c7b1813b2a95d12bacf12414b91096
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-12-06 01:49:51 -0500

    feat: views

---
+++ src/views/index.ts
@@ -0,0 +1,8 @@
+export { HomePage } from "./HomePage";
+export type { HomePageProps } from "./HomePage";
+
+export { BlogListPage } from "./BlogListPage";
+export type { BlogListPageProps } from "./BlogListPage";
+
+export { ContentCalendarPage } from "./ContentCalendarPage";
+export type { ContentCalendarPageProps } from "./ContentCalendarPage";
