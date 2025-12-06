c32666253b57e475bb88ef0a826d0cb222ecf442
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-12-06 01:48:31 -0500

    refactor: hook exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
 export { usePageTitle } from "./usePageTitle";
-export { useTheme, ThemeProvider } from "./useTheme";
+export { useTheme } from "./useTheme";
 export { useReadingProgress } from "./useReadingProgress";
