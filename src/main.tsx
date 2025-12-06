fc823cb0eb4368bbcbc91ba440ef074e42c265ce
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-12-06 03:24:40 -0500

    fix: remove app router and context

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@/hooks";
 import { AppShell } from "./AppShell";
 import "@/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <AppShell />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AppShell />
   </React.StrictMode>
 );
